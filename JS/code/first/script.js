209df47869b7d8c2f70a06c6a158d85fe9d2134e
prasad-bigdp <bigdp585@gmail.com>
2024-05-27 10:57:29 +0530

    first commit

---
+++ JS/code/first/script.js
@@ -0,0 +1,40 @@
+// var firstName = "raj" // This is a single line comment
+// /*
+// vnvnbvbn
+// bvcbvcbv
+// dbgfcbv
+// */
+// var b = 30
+// var c = 2
+
+// console.log("Hello world" + (c + b))
+var a=20
+alert("This is my alert");
+var result = confirm("Are you sure?")
+console.log(result)
+var a = prompt("Enter value for a")
+var b= prompt("Enter value of b")
+alert(a+b)
+
+/* using prompts and alerts write a basic js to take two values and alert their sum*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
