6550ef048eb619540b3691818e2a2d1023786e9d
prasad-bigdp <bigdp585@gmail.com>
2024-05-28 10:44:33 +0530

    first commit

--- JS/code/first/script.js
+++ JS/code/first/script.js
@@ -8,15 +8,113 @@
 // var c = 2
 
 // console.log("Hello world" + (c + b))
-var a=20
-alert("This is my alert");
-var result = confirm("Are you sure?")
-console.log(result)
-var a = prompt("Enter value for a")
-var b= prompt("Enter value of b")
-alert(a+b)
+// var a=20
+// alert("This is my alert");
+// var result = confirm("Are you sure?")
+// console.log(result)
+// var a = +prompt("Enter value for a")
+// var b= +prompt("Enter value of b")
+// alert(a+b)
 
 /* using prompts and alerts write a basic js to take two values and alert their sum*/
+console.log(a) // undefined
+// var a = 50;
+// console.log(a)
+// var b = 50;
+// var c = 30;
+// function fun ()
+// {
+//     console.log(x)
+//     var x = 35;
+// }
+// fun()
+console.log(a)// it prints undefined, var allows hoisting
+var a = 50;
+var a = 30; // var allows re declaration
+a = 55; // var also allows re assigns
+// console.log(b)// Cannot access 'b' before initialization
+let b = 65;
+ b = 75; // let cannot re declare but reassign is okay
+
+const c = 25;
+// redeclare is not possible
+
+function fun ()
+{
+     x = 25;
+    if (true)
+    {
+        var y = 35;
+        let z = 20;
+    }
+    console.log(y)
+}
+fun()
+
+let str = "prasad"
+console.log(typeof (str))
+str = 35;
+console.log(typeof str)
+str = true;
+console.log(typeof str)
+console.log(2+2)
+console.log(5 + "4")
+console.log(5-"4")
+console.log(5-"a")
+let p=20
+if (1 < p < 3)
+{
+    console.log("success")
+}
+else
+{
+    console.log("fail")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
